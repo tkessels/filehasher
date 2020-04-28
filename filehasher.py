3859681f7d24943ae23b91d5087b3f7309a99af2
TKE <tobias.kessels@certbw.de>
2020-04-28 13:58:41 +0200

    Add argument parsing and max_size filtering

--- filehasher.py
+++ filehasher.py
@@ -2,25 +2,38 @@ import os
 import hashlib
 import gzip
 import stat
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--max-file-size", help="Max Size of files to hash in bytes", type=int, default=1073741824, required=False)
+parser.add_argument("-v", "--verbosity", action="count", default=0,help="increase output verbosity", required=False)
 
+args = parser.parse_args()
 
+def log(message,loglevel=2):
+    level=["error","info","debug"]
+    if loglevel < args.verbosity:
+        print("[{}] : {}".format(level[loglevel],message.rstrip()))
 
 def get_file_hash(file):
+    hash=None
     try:
-        with open(file,'rb') as data:
-            return hashlib.md5(data.read()).hexdigest()
+        if os.path.isfile(file) and os.access(file,os.R_OK):
+            if os.path.getsize(file)<args.max_file_size:
+                with open(file,'rb') as data:
+                    hash=hashlib.md5(data.read()).hexdigest()
     except Exception:
-        print("could not hash file {}".format(file))
-        return None
+        log("could not access file {}".format(file),0)
+    return hash
 
 
 walker=os.walk('/')
 with gzip.open('./md5hashes.txt.gz', 'wt') as outfile:
     for path,folders,files in walker:
+        log("processing path {}".format(path),2)
         for file in [os.path.join(path,f) for f in files]:
-            if (os.path.isfile(file) and os.access(file,os.R_OK) ):
-                hash=get_file_hash(file)
-                if hash:
-                    output="{}  {}\n".format(hash,file)
-                    outfile.write(output)
+            hash=get_file_hash(file)
+            if hash:
+                output="{}  {}\n".format(hash,file)
+                log(output,1)
+                outfile.write(output)
